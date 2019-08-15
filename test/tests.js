ac70adc07d4ac26fb33bd9d540e26cd8152511d1
allisonbusse <allisonbusse@gmail.com>
2019-08-15 13:59:24 -0700

    more travis

--- test/tests.js
+++ test/tests.js
@@ -1,8 +1,6 @@
 import './html-equal.js';
 import './html-equal.test.js';
 import './html-to-dom.test.js';
-import './render-image.test.js';
-import './ImageItem.test.js';
 // // individual test files...
 // import './whatever.test.js';
  
