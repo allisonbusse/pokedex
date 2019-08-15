0e26091997566316beec366c2a84dd60dae3dd7e
allisonbusse <allisonbusse@gmail.com>
2019-08-15 11:01:41 -0700

    initial setup

---
+++ test/tests.js
@@ -0,0 +1,8 @@
+import './html-equal.js';
+import './html-equal.test.js';
+import './html-to-dom.test.js';
+import './render-image.test.js';
+import './ImageItem.test.js';
+// // individual test files...
+// import './whatever.test.js';
+ 
